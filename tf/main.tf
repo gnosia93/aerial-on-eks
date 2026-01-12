1ff520b556acea9c18e940faee6b3080b16396c0
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-12 23:08:56 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -90,7 +90,7 @@ resource "aws_route_table_association" "private" {
 # ------------------------------------------------
 
 resource "aws_iam_role" "eks_creator_role" {
-  name = "GetStartedEKS_Role"
+  name = "AerialOnEKS_Role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
